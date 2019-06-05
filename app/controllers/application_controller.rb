e81671e18dc0dc5c56ee18a463d71987663ff05a
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-06-05 17:11:08 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
   post '/articles' do
     Article.create(title: params[:title], content: params[:content])
 
-    redirect to "/articles/#{"
+    redirect to "/articles/#{ @article.id }"
   end
 
   get '/articles' do
